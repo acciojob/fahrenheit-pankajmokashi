83c1067e88b5390228b22987447f5d41350a1592
Pankaj Mokashi <127533657+pankajmokashi@users.noreply.github.com>
2023-04-24 18:54:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //your JS code here. If required.
+let celsius = prompt()
 function toFahrenheit(celsius){
 	let ans = celsius * 1.8 + 32
-	return ans
+	alert(ans)
 }
-
-export.toFahrenheit
+toFahrenheit(celsius)
